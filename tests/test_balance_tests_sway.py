338dd22393b40a764c7d68c4914cfecd10675b0e
matteo-serafino <matteo.serafino1+github@gmail.com>
2026-04-08 21:33:44 +0200

    add: sway test

---
+++ tests/test_balance_tests_sway.py
@@ -0,0 +1,38 @@
+import os
+import pytest
+from datetime import datetime
+
+from numpy import arange, zeros
+from pandas import read_csv
+from physiodsp.sensors.imu.accelerometer import AccelerometerData
+from physiodsp.balance_tests.sway import Sway
+
+test_folder_path = os.path.dirname(os.path.realpath(__file__))
+
+
+@pytest.mark.parametrize(
+        "fs",
+        [
+            (25),
+        ]
+)
+def test_balance_test_sway(fs):
+
+    df = read_csv(os.path.join(test_folder_path, "sway.csv"), usecols=["x", "z"])
+    n_samples = len(df)
+
+    timestamp_start = datetime.now().timestamp()
+    timestamps = timestamp_start + arange(start=0, step=1/fs, stop=int(n_samples/fs))
+
+    accelerometer = AccelerometerData(
+        timestamps=timestamps,
+        x=df.x.values[:len(timestamps)],
+        y=zeros(len(timestamps)),
+        z=df.z.values[:len(timestamps)],
+        fs=fs
+    )
+
+    processor = Sway().run(accelerometer=accelerometer, sensor_height=1.4)
+
+    metrics = processor.biomarker
+    assert metrics is not None
