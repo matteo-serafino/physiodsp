dedd62a89baa2b31ea0cfd8734c6bfab3bd90ce1
matteo-serafino <matteo.serafino1+github@gmail.com>
2026-05-18 09:17:52 +0200

    change: refactor PIM

--- physiodsp/activity/pim.py
+++ physiodsp/activity/pim.py
@@ -1,8 +1,14 @@
 from numpy import abs
 from pandas import DataFrame
+from pydantic import BaseModel
 
 from physiodsp.base import BaseAlgorithm
 from physiodsp.sensors.imu.base import IMUData
+
+
+class PIMSettings(BaseModel):
+    """PIM Algorithm Settings"""
+    aggregation_window: int = 5
 
 
 class PIMAlgorithm(BaseAlgorithm):
@@ -10,13 +16,27 @@ class PIMAlgorithm(BaseAlgorithm):
 
     _algorithm_name = "PIMAlgorithm"
     _version = "0.1.0"
-    _aggregation_window = 5
+
+    def __init__(self,
+                 settings: PIMSettings = PIMSettings()
+                 ) -> None:
+        self.settings = settings
+        self._aggregation_window = settings.aggregation_window
+        return None
 
     def run(self, data: IMUData):
-
+        self.data = data
         self.values_x = abs(data.x)
         self.values_y = abs(data.y)
         self.values_z = abs(data.z)
+
+        # Raw biomarker: combine absolute values for raw signal representation
+        self.biomarker = DataFrame({
+            'timestamps': data.timestamps,
+            'x': self.values_x,
+            'y': self.values_y,
+            'z': self.values_z
+        })
 
         return self
 
@@ -24,12 +44,7 @@ class PIMAlgorithm(BaseAlgorithm):
                   method: str = 'sum'
                   ):
 
-        df = DataFrame({
-            'timestamps': self.data.timestamps,
-            'x': self.values_x,
-            'y': self.values_y,
-            'z': self.values_z
-        })
+        df = self.biomarker.copy()
 
         df['timestamps'] = df['timestamps'] // self.aggregation_window
 
